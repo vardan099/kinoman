e9e3cd0636971a53f242fa003f92851bd63a6f51
vardan099 <galstyan099@gmail.com>
2019-08-03 23:32:37 +0400

    Added node-sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Provider} from 'react-redux'
 import configureStore from './store';
-import './App.css';
+import './App.scss';
 import HomePage from './components/HomePage/HomePage'
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
